f01ea37360f886c58c42d2b5f69ab7789df9e2af
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2018-05-01 15:34:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var formTemplate = Handlebars.compile(document.getElementById('recipe-form-template').innerHTML);
+  var result = formTemplate();
+  document.getElementById('centeredDiv').innerHTML += result;
+  Handlebars.registerHelper('displayIngredient', function(){
+    return new Handlebars.SafeString("<li>" + this.ingredient + "</li>");
+  })
+}
+
+function createRecipe()
+{
+    var result = document.getElementById("end-process-message").innerHTML += "Recipe created!!!";
+    return result;
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
